420f9f9a4b81aa52ea31bd70d17ca354101afb44
Angela Trujillo <angelitha_1002@hotmail.com>
2022-02-10 15:54:04 -0500

    add word buzz in multiples of five

--- ejercicio-a.js
+++ ejercicio-a.js
@@ -1,6 +1,8 @@
 for (let i = 0; i <= 100; i++) {
     if  (i % 2 == 0){
         console.log(i,'buzz')
+    } else if (i % 5 == 0) {
+        console.log(i, 'bazz')
     } else {
         console.log(i)
     }
