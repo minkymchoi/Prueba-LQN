e9d586f88a52e97d9e8dce0132dfd478699ac665
Angela Trujillo <angelitha_1002@hotmail.com>
2022-02-11 23:44:58 -0500

    fixed bug in header css

--- fanpage_starwars/src/components/header.js
+++ fanpage_starwars/src/components/header.js
@@ -1,10 +1,9 @@
-import { Col, Row } from 'antd';
 import Header from '../assets/img/Title.jpeg';
 
 const HeaderNav = () => {
 
   return (
-    <div className='header-background col-img-6'>
+    <div className='black-background'>
         <div className='text-center'>
           <img src={Header} width="200" height="100" class="img-responsive center-block"/>
         </div>
