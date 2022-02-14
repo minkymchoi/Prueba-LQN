4ed1a1d3be6d1dd2f53cfeff8e1efc29f7940cc4
Angela Trujillo <angelitha_1002@hotmail.com>
2022-02-14 13:45:30 -0500

    delete comment

--- fanpage_starwars/src/api/api.js
+++ fanpage_starwars/src/api/api.js
@@ -7,7 +7,6 @@ import {
 
 const httpLink = createHttpLink({
     uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
-    //fetchOptions:{mode:'no-cors'},
 })
 
 const client = new ApolloClient({
